b6ba6dd06442efbbf78315d05602357ee43a82f8
Fiitkar <achverca@gmail.com>
2017-03-31 18:25:09 +0200

    Add new directory with a lot of very useful stuff into /Pravdepodobnosť_a_štatistika/skusky/skusky_dobra_priprava_2016/

---
+++ 3._Semester/Pravdepodobnosť_a_štatistika/skusky/skusky_dobra_priprava_2016/Skusky/PAS-skuska-2015/Testy a skúšanie_files/img.pl
@@ -0,0 +1,268 @@
+body {
+    font-family: verdana, arial, helvetica, sans, sans-serif;
+    font-size: 13px;
+    background: white;
+    color: #333;
+    text-align: left;
+    vertical-align: middle;
+    padding: 0;
+padding-bottom: 30px;
+    margin:0 3px;
+}
+
+a{
+    color: #392983;
+    text-decoration: none;
+}
+
+a:hover{
+    text-decoration: underline;
+}
+
+h1{
+    font-size: 15px;
+    margin:5px 0 0 0;
+    background:#e7ebed url('/img.pl?userunid=162204') left top no-repeat; 
+    color:#464646; line-height:21px;
+    height:21px; padding:0 0 0 25px;
+}
+
+li{
+    list-style:none;
+    background: url('/img.pl?userunid=153870') no-repeat;
+    background-position: 0px 5px;
+    margin:1px 0px 1px 8px; padding-left:15px;
+}
+
+ul{
+  margin:10px 0 10px 10px; padding:0;
+}
+
+tr.sudy{
+    background-color: #dcdcdc;
+}
+
+tr.zahlavi, td.zahlavi{
+    padding-left: 5px;
+    padding-right: 5px;
+    background-color: #e7ebed;
+    color: #464646;
+}
+
+pre{
+    margin-top: 0;
+    margin-bottom: 0;
+}
+
+span.small, div.small{
+    color: #555;
+}
+
+div.small {
+    font-size:15px;
+    text-align: justify;
+}
+
+.servis{
+    text-align: center;
+    padding-left: 5px;
+    padding-right: 5px;
+    background-color: #f6e071;
+    color: black;
+    font-weight: bold;
+}
+
+.focused{
+    background-color: #c0c0c0;
+}
+
+hr{
+     width:100%;
+     height:1px;
+     background: #c0c0c0;
+     text-align:left;
+     margin:5px 0 8px 0; border:0; padding:0;
+}
+
+hr.cleaner {
+      clear:both;
+      height:1px;
+      margin: -1px 0 0 0; padding:0;
+      border:none;
+      visibility: hidden;
+}
+
+.portlet, .sideportlet {
+    padding:3px;
+    background-color:white;
+    border: 1px solid #c0c0c0;
+    margin:5px 0;
+}
+
+#hlavicka{
+  position:relative;
+  height:106px;
+  background: url('/img.pl?userunid=162189') repeat-x;
+  min-width:833px;
+  _width: expression(document.body.clientWidth < 833? "833px" : "auto" );
+}
+
+#ie1{
+  position:relative;
+  height:79px;
+  background:url('/img.pl?userunid=162188') no-repeat;
+}
+ 
+#menu{
+  height:25px; _height:23px;
+  background: url('/img.pl?userunid=162190') repeat-x;
+  color:#000000;
+  padding:1px;
+  font-size:11px;
+}
+
+#svatek, #log, #ema{
+  float:left;
+  height:25px;
+  line-height:22px;
+  padding:0 2px;
+}
+
+#svatek img, #log img, #ema img{
+  vertical-align:middle;
+}
+
+#ema a{
+  color:#000000;
+}
+
+#base{
+  margin:0 5px;
+}
+
+#base-right{
+  text-align:left;
+
+}
+
+#ikonky{
+  float:right;
+  padding-top:2px;
+}
+
+#vlajky{
+  position:absolute;
+  right:5px; top:5px
+}
+
+#ais{
+  position:absolute;
+  right:5px; top:34px;
+  font-size:11px; font-weight:bold;
+  color: #ffffff;
+}
+
+a#svf{
+position:absolute; 
+display:block; 
+width:41px; height:50px; 
+top:15px; left:308px;
+}
+
+a#sjf{
+position:absolute; 
+display:block; 
+width:41px; height:50px; 
+top:15px; left:364px;
+}
+
+a#fei{
+position:absolute; 
+display:block; 
+width:41px; height:50px; 
+top:15px; left:420px;
+}
+
+a#fchpt{
+position:absolute; 
+display:block; 
+width:41px; height:50px; 
+top:15px; left:476px;
+}
+
+a#fa{
+position:absolute; 
+display:block; 
+width:41px; height:50px; 
+top:15px; left:532px;
+}
+
+a#mtf{
+position:absolute; 
+display:block; 
+width:41px; height:50px; 
+top:15px; left:588px;
+}
+
+a#fiit{
+position:absolute; 
+display:block; 
+width:41px; height:50px; 
+top:15px; left:644px;
+}
+
+a#svf:hover, a#sjf:hover, a#fei:hover, a#fchpt, a#fa, a#mtf, a#fiit{
+text-decoration:none;
+}
+
+#footer-panel{
+color: #000000;
+width: 100%;
+height: 30px;
+bottom: 0px;
+position: fixed;
+display: block;
+}
+#footer-panel a{
+color: #000000;
+}
+#panel-bg{
+background: url('/img.pl?userunid=162220') no-repeat transparent;
+width: 600px;
+height: 30px;
+display: block;
+bottom: 0px;
+margin-left:auto;
+margin-right:auto;
+}
+#user-name{
+width: 300px;
+float:left;
+position: relative;
+top: 10px;
+left: 80px;
+}
+.line-cutter{
+position:relative;
+bottom: 0px;
+}
+#user-panel{
+float:right;
+position:relative;
+top: 2px;
+right: 20px;
+}
+#user-panel img{
+position:relative;
+top:5px;
+margin: 0 2px;
+}
+#user-panel a{
+color: #000000;
+}
+#panel-container{
+width: 100%;
+display: table;
+margin-left:auto;
+margin-right:auto;
+} 
